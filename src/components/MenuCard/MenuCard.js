f19674e35e64c98976553d657ec4997b65caa873
jylee <wjddp3@naver.com>
2021-07-27 22:03:12 +0900

    Refactor MenuCard component
    - separate sub component to MenuItem
    - add disabled props

--- src/components/MenuCard/MenuCard.js
+++ src/components/MenuCard/MenuCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import MenuItem from './MenuItem';
 
 import styled from 'styled-components';
-import Colors from 'datas/Colors';
 
 const Menu = styled.View`
   background-color: white;
@@ -11,6 +11,10 @@ const Menu = styled.View`
   margin-bottom: 20px;
 `;
 
+const Divider = styled.View`
+  height: 1px;
+  background-color: #e0e0e0;
+`;
 const MenuTitle = styled.Text`
   font-size: 20px;
   font-weight: bold;
@@ -18,62 +22,16 @@ const MenuTitle = styled.Text`
   letter-spacing: 1px;
 `;
 
-const MenuItem = styled.Pressable`
-  padding: 10px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  height: auto;
-`;
-
-const Divider = styled.View`
-  height: 1px;
-  background-color: #e0e0e0;
-`;
-
-const Wrapper = styled.View`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  flex: 1;
-  margin-left: 5px;
-`;
-const MenuItemName = styled.Text`
-  color: ${Colors.fontColor};
-`;
-
-const Description = styled.Text`
-  margin-top: 5px;
-  font-size: 13px;
-  color: #696969;
-`;
-
-const GroupCard = ({group, navigation}) => {
+const MenuCard = ({group, navigation}) => {
   const {groupName, menuItems} = group;
 
   return (
     <Menu elevation={2}>
       {groupName && <MenuTitle> {groupName} </MenuTitle>}
-      {menuItems.map((group, i) => {
-        const {name, icon, routeTo, children, description, callback} = group;
+      {menuItems.map((item, i) => {
         return (
           <>
-            <MenuItem
-              android_ripple={{color: 'lightgray'}}
-              onPress={() => {
-                routeTo &&
-                  navigation.navigate(routeTo, {title: name, children});
-                callback && callback();
-              }}>
-              <Icon name={icon} size={25} color={Colors.fontColor} />
-              <Wrapper>
-                <MenuItemName> {name}</MenuItemName>
-                {description && <Description> {description}</Description>}
-              </Wrapper>
-              {routeTo && (
-                <Icon name="chevron-right" size={25} color="#9e9e9e" />
-              )}
-            </MenuItem>
+            <MenuItem item={item} navigation={navigation} />
             {i !== menuItems.length - 1 && <Divider />}
           </>
         );
@@ -82,4 +40,4 @@ const GroupCard = ({group, navigation}) => {
   );
 };
 
-export default GroupCard;
+export default MenuCard;
