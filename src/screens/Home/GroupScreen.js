7267294dcb3d3422cd54468f408d52c6fd0819ba
jylee <wjddp3@naver.com>
2021-08-04 00:26:33 +0900

    Add loader in group screen while loading posts

--- src/screens/Home/GroupScreen.js
+++ src/screens/Home/GroupScreen.js
@@ -1,14 +1,15 @@
-import React, {useEffect} from 'react';
-import {FlatList} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator} from 'react-native';
 import {HeaderBackButton} from '@react-navigation/stack';
 import moment from 'moment';
 
 import Header from 'components/Header';
 import {FloatingButton} from 'components/common';
 import Post from 'components/Post';
+import Empty from 'components/Empty/Empty';
 
 import {usePosts} from 'hooks';
-
+import {EMPTY_CONTENT} from 'constants/App';
 import styled from 'styled-components';
 import Colors from 'datas/Colors';
 
@@ -81,6 +82,16 @@ const Dot = styled.View`
   background-color: white;
 `;
 
+const Loader = styled.View`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  align-items: center;
+  justify-content: center;
+`;
+
 const PostView = ({post, onPress}) => {
   return (
     <PostWrapper>
@@ -107,13 +118,15 @@ const GroupScreen = ({route, navigation}) => {
     group: {id, name},
   } = route.params;
   const {postList, getPostList} = usePosts(id);
+  const [isLoading, setIsLoading] = useState(true);
 
   console.log('<GroupScreen/> groupId : ', id);
 
   useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
+    const unsubscribe = navigation.addListener('focus', async () => {
       console.log('>>postlist<<', postList);
-      getPostList(id);
+      await getPostList(id);
+      setIsLoading(false);
     });
     return unsubscribe;
   }, [navigation]);
@@ -130,9 +143,14 @@ const GroupScreen = ({route, navigation}) => {
         headerTitle={name}
       />
       <Wrapper>
-        <Divider />
+        {postList.length > 0 && <Divider />}
         <PostList
           data={postList}
+          contentContainerStyle={{
+            flex: 1,
+            justifyContent: 'center',
+          }}
+          ListEmptyComponent={!isLoading && <Empty message={EMPTY_CONTENT} />}
           renderItem={({item}) => (
             <PostView
               post={item}
@@ -148,6 +166,11 @@ const GroupScreen = ({route, navigation}) => {
           numColumns={1}
           keyExtractor={(item, index) => index}
         />
+        {isLoading && (
+          <Loader>
+            <ActivityIndicator size="large" color={Colors.active} />
+          </Loader>
+        )}
       </Wrapper>
       <FloatingButton
         onPress={() => navigation.navigate('PostDetail', {groupId: id})}
